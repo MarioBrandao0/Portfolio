4661cdc368cf8f8c5ea233fa1ef753b4343bd05f
Mário Brandão <mariorochedo329@gmail.com>
2024-11-25 16:30:18 +0000

    Terminar de fazer Habilidades

--- Porfolio/src/components/Habilidades.jsx
+++ Porfolio/src/components/Habilidades.jsx
@@ -1,9 +1,18 @@
 import '../styles/Habilidades.css'
+import PythonIMG from '../assets/Python.png'
 function Habilidades() {
     //Desenvolver isso aqui
     return (
         <div className="Habilidades">
-            <p>oi</p>
+            <button className="python">
+                <img src={PythonIMG} alt="python" />
+                <strong>Python</strong>
+            </button>
+
+            <div className="JavaScript">
+                <p>oi</p>
+            </div>
+
         </div>
     )
 }
