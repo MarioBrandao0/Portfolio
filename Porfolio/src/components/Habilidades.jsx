a8df1b8f980a7700de16cc718cac91ee7b5dca96
Mário Brandão <mariorochedo329@gmail.com>
2024-11-26 18:56:48 +0000

    Desenvolver o script de habilidades

--- Porfolio/src/components/Habilidades.jsx
+++ Porfolio/src/components/Habilidades.jsx
@@ -1,10 +1,23 @@
 import '../styles/Habilidades.css'
+import { useRef } from 'react'
 import PythonIMG from '../assets/Python.png'
+
+
 function Habilidades() {
+    const dialog = useRef(null)
+    function chamarHB() {
+        dialog.current.showModal()
+    }
+
+    function fecharHB(){
+        dialog.current.close()
+    }
+
+    
     //Desenvolver isso aqui
     return (
         <div className="Habilidades">
-            <button className="python">
+            <button className="python" onClick={chamarHB}>
                 <img src={PythonIMG} alt="python" />
                 <strong>Python</strong>
             </button>
@@ -12,6 +25,20 @@ function Habilidades() {
             <div className="JavaScript">
                 <p>oi</p>
             </div>
+        
+
+            <dialog className='dialogHB' ref={dialog}>
+                <div className="Linguagens">
+                    <img src='' alt="" />
+                    
+                </div>
+                <div className="Conhecimentos">
+                    <p>
+                        Desenvolver isso de Habilidades
+                    </p>
+                </div>
+                <button onClick={fecharHB}>Close</button>
+            </dialog>
 
         </div>
     )
